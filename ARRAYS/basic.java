ba2a2227348e563cab8a2ea87f81a58f97cbaf18
SACHIN KUMAR <sachin40x@gmail.com>
2025-12-09 10:46:25 +0530

    arrays

---
+++ ARRAYS/basic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class basic {
+    public static void main(String[] args) {
+        int marks[] = new int [100];
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+        System.out.println("phy: " + marks[0]);
+          System.out.println("chem: " + marks[1]);
+            System.out.println("math: " + marks[2]);
+
+        marks[2] = 100;
+        System.out.println("math : " + marks[2]);
+    }
+}
